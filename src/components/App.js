9513536d997b6bc16b7e92de220f54c4f883ae26
ibouelezz <ibrahim.ali.aboelezz@gmail.com>
2021-06-29 15:10:30 +0200

    redux boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import LoadingBar from 'react-redux-loading'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Starter Code
+      </div>
+    )
+  }
+}
+
+export default App
