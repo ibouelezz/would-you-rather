1269e6ea1161adcc79d5b5c64eca95d13991a30b
ibouelezz <ibrahim.ali.aboelezz@gmail.com>
2021-06-29 15:16:14 +0200

    load initial data

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react'
-import LoadingBar from 'react-redux-loading'
+import React, { Component } from "react";
+import { handleInitialData } from "../actions/shared";
+import { connect } from "react-redux";
+import LoadingBar from "react-redux-loading";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
   render() {
     return (
       <div>
+        <LoadingBar />
         Starter Code
       </div>
-    )
+    );
   }
 }
 
-export default App
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+
+export default connect(mapStateToProps)(App);
