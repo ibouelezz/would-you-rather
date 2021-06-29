579c1c1d5f34fe30fb417452c4843fd8e65a74ed
ibouelezz <ibrahim.ali.aboelezz@gmail.com>
2021-06-29 16:37:17 +0200

    add dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { handleInitialData } from "../actions/shared";
 import { connect } from "react-redux";
 import LoadingBar from "react-redux-loading";
 import Login from "./Login";
+import Dashboard from "./Dashboard";
 
 class App extends Component {
   componentDidMount() {
@@ -12,7 +13,7 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        <Login />
+        <Dashboard />
       </div>
     );
   }
